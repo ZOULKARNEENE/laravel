9694e27cec0e3dcb9c15c47414a08428fd3e5afc
Zoulkarneene <zoulkarneenen@gmail.com>
2026-03-09 02:29:43 +0000

    mise a jour du projet

--- resources/views/etudiant.blade.php
+++ resources/views/etudiant.blade.php
@@ -1,13 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
- <h5>LA LISTE DES ETUDIANTS</h5>
-    
-</body>
-</html>
+@extends("layouts.master")
+
+@section("contenu")
+
+ <div class="my-3 p-3 bg-body rounded shadow-sm">
+    <h3 class="border-bottom pb-2 mb-4">Liste des etudiants inscrits</h3>
+
+    <div class="mt-4">
+        <div class="d-flex justify-content-end mb-4">
+            <a href="#" class="btn btn-primary">Ajouter un etudiant</a>
+        </div>
+    <table class="table table-bordered table-hover ">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nom</th>
+      <th scope="col">Prénom</th>
+      <th scope="col">Class</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+         <td >
+          <a href="#" class="btn btn-info">Editer</a>
+          <a href="#" class="btn btn-danger">Supprimer</a>
+            </td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+            <td>
+            <a href="#" class="btn btn-info">Editer</a>
+            <a href="#" class="btn btn-danger">Supprimer</a>
+            </td>
+    </tr>
+
+  </tbody>
+</table>
+</div>
+
+  </div>
+
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name("acceuil");
 
- Route::get('/etudiant',[EtudiantController::class,'index']);
+ Route::get('/etudiant',[EtudiantController::class,'index'])->name("etudiant");
